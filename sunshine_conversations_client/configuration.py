5f636174df8b68f3dd4b9c5ac0423e08db4d0639
zendesk-ops-ci <zendesk-ops-ci@zendesk.com>
2021-04-12 15:30:37 +0000

    Generate version 9.4.4

--- sunshine_conversations_client/configuration.py
+++ sunshine_conversations_client/configuration.py
@@ -358,7 +358,7 @@ conf = sunshine_conversations_client.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 9.4.3\n"\
-               "SDK Package Version: 9.4.3".\
+               "SDK Package Version: 9.4.4".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
