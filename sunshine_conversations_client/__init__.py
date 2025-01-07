38ead3ddb01656ab1dce59f5752e4ab716aa444c
zendesk-ops-ci <zendesk-ops-ci@zendesk.com>
2025-01-07 18:04:37 +0000

    Generate version 14.3.1

--- sunshine_conversations_client/__init__.py
+++ sunshine_conversations_client/__init__.py
@@ -12,7 +12,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "14.3.0"
+__version__ = "14.3.1"
 
 # import apis into sdk package
 from sunshine_conversations_client.api.activities_api import ActivitiesApi
